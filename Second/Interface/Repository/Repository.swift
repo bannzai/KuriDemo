9427535cd44fbd1415f26ec89b33e3806331f071
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-04-06 06:22:58 +0900

    Add Second Template

---
+++ Second/Interface/Repository/Repository.swift
@@ -0,0 +1,11 @@
+//
+//  __REPOSITORY__.swift
+//  Kuri
+//
+//  Created by __USERNAME__ on __DATE__.
+//  Copyright © 2016年 __USERNAME__. All rights reserved.
+//
+
+protocol __REPOSITORY__ {
+    func fetch(_ closure: (__ENTITY__) -> Void) throws 
+}
